9b1d18e26bd4f29c4d59f832e850383eed00d19d
RileyGatto <rmgatto64@gmail.com>
2026-03-05 22:08:10 +1100

    UIDone

--- Pass-Task-2.1/app/src/main/java/com/example/pass_task_21/MainActivity.java
+++ Pass-Task-2.1/app/src/main/java/com/example/pass_task_21/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.pass_task_21;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,36 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner fromSpinner;
+    Spinner toSpinner;
+    EditText inputTxt;
+    Button convertButton;
+    TextView output;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        fromSpinner = findViewById(R.id.spinner_from);
+        toSpinner = findViewById(R.id.spinner_too);
+        inputTxt = findViewById(R.id.user_input);
+        convertButton = findViewById(R.id.convert_button);
+        output = findViewById(R.id.result_text);
+
+        String[] units = getResources().getStringArray(R.array.options);
+
+
+
+
+
     }
 }
